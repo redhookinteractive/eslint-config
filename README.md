c2bc0fc56a6b7552418084952432b74593d0514b
Logan Sotelo <lsotelo@polarislabs.com>
2022-06-09 13:20:52 -0400

    Updating the readme.md, switching to yarn install, adding react.js export, added empty react-native

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# eslint-config
+# @redhook/eslint-config
+
+An eslint configuration that will allow projects to have consistent code quality and standards. It exports configurations for node, react, react-native, and a common package that can be used more generally.
+
+## How to use it
+
+### Install the package
+
+```bash
+# from the project you're trying to add it to
+yarn add @redhook/eslint-config -D
+```
+
+### Add the package in your .eslintrc.js file
+
+To install the common package:
+
+```js
+module.exports = {
+  extends: [
+    '@redhook/eslint-config',
+  ],
+};
+```
+
+To install the node package:
+
+```js
+module.exports = {
+  extends: [
+    '@redhook/eslint-config/node',
+  ],
+};
+```
+
+To install the react package:
+
+```js
+module.exports = {
+  extends: [
+    '@redhook/eslint-config/react',
+  ],
+};
+```
+
+To install the react-native package:
+
+```js
+module.exports = {
+  extends: [
+    '@redhook/eslint-config/react',
+  ],
+};
+```
---
+++ react.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  extends: ["airbnb", "./common"],
+  overrides: [
+    {
+      extends: ["eslint:recommended", "plugin:prettier/recommended"],
+      files: ["*.js"],
+      parser: "espree",
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module",
+      },
+      plugins: ["prettier"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "import/no-default-export": "error",
+    "jsx-a11y/anchor-is-valid": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "react/forbid-prop-types": "warn",
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-unused-prop-types": "warn",
+    "react/require-default-props": "off",
+  },
+};
