1d01704b54eff3b1b28b2560d516199f988087f1
Logan Sotelo <lsotelo@polarislabs.com>
2022-07-03 15:53:02 -0400

    Another attempt

--- react-native.js
+++ react-native.js
@@ -1,4 +1,7 @@
 module.exports = {
-  extends: ["./react"],
+  env: {
+    "react-native/react-native": true,
+  },
+  extends: ["./react", "plugin:react-native/all"],
   plugins: ["react-native"],
 };
